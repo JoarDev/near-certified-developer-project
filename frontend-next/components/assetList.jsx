4cf4e7ba1c7f43d96edbbfe7b2ded3771d4200f2
Joaquin Ignacio Ramos <j.ramos5452@gmail.com>
2022-10-21 20:15:22 +0000

    get buyers

--- frontend-next/components/assetList.jsx
+++ frontend-next/components/assetList.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react"
 import { assetContract } from "../lib/contract"
 
 export const AssetList = ({ assetList, userId }) => {
+    const [value, setValue] = useState(null)
 
     const handleParticipate = (uuid) =>{
-        assetContract.setBuyAsset({asset_uuid: uuid, buyer_name: userId})
+        assetContract.setBuyAsset({buyer_name: userId, asset_uuid: uuid})
+    }
+
+    const handleGetOwners = (uuid) => {
+        assetContract.getBuyersAssets({uuid: uuid})
+        .then((body) => {
+            setValue(body)
+            console.log()
+        })
     }
 
     return (
@@ -17,11 +27,29 @@ export const AssetList = ({ assetList, userId }) => {
                         <div>Monto. {value.ammount}</div>
                         <div>Dueño: {value.owner_id}</div>
                         <div>Tipo: {value.type}</div>
-                        <button onClick={() => handleParticipate(value.uuid)}>Comprar activo</button>
+                        <button onClick={() => handleParticipate(value.uuid)} style={{marginRight: 10}}>Comprar activo</button>
+                        <button onClick={() => handleGetOwners(value.uuid)}>Ver compradores</button>
                     </div>
                 ))
             }
             </div>
+            {
+                value != null ? (
+                    <div>
+                        <h4>Compradores del activo:</h4>
+                        <div style={{display: 'flex', flexDirection: 'row', gap: 20, paddingBottom: 40}}>
+                        {
+                            value.map((buyer) => (
+                                <div>
+                                    <div>Nombre: {buyer.name}</div>
+                                    <div>Cuenta: {buyer.address}</div>
+                                </div>
+                            ))
+                        }
+                        </div>
+                    </div>
+                ) : null
+            }
         </div>
     )
 }
